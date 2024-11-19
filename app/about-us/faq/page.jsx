c26a303e45714ee670e07390008c432125cdb038
ZafarNorkulov <norqulovzafar123@gmail.com>
2024-11-19 16:02:39 +0500

    faq page fixed

--- app/about-us/faq/page.jsx
+++ app/about-us/faq/page.jsx
@@ -5,6 +5,7 @@ import HomeBlogs from "@/components/sections/HomeBlogs";
 import Question from "@/assets/images/_.png"
 import { getFaqs } from "@/app/api/blogs/blogs";
 import Image from "next/image";
+import SwiperSection from "@/components/SwiperSection";
 
 
 const FAQPage = () => {
@@ -27,9 +28,9 @@ const FAQPage = () => {
       <div className="max-container mb-[100px] pt-[100px] md:mb-[150px] md:pt-[150px]">
         <h2 className="section-title mb-[30px]">Вопрос-ответ</h2>
 
-          <div className="mx-auto max-w-[1140px]">
-        <div className="flex items-center gap-4">
-          <Image src={Question} width={30} height={30} alt="img" />
+        <div className="mx-auto max-w-[1140px]">
+          <div className="flex items-center gap-4">
+            <Image src={Question} width={30} height={30} alt="img" />
             {
               faqs?.map((faq) => (
                 <Accordion key={faq.id} title={faq.title} description={faq.description} />
@@ -38,7 +39,10 @@ const FAQPage = () => {
           </div>
         </div>
       </div>
-
+      <div className="mx-[177px]">
+        <h2 className="section-title my-[100px]">Актуальные проекты</h2>
+        <SwiperSection />
+      </div>
       <HomeBlogs />
     </>
   );
