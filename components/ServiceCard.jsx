9600c898963809a192655b3f823015ecddc687de
ZafarNorkulov <norqulovzafar123@gmail.com>
2025-03-05 20:04:54 +0500

    change

--- components/ServiceCard.jsx
+++ components/ServiceCard.jsx
@@ -1,13 +1,15 @@
-const ServiceCard = ({ title, description }) => {
+import Link from "next/link";
+
+const ServiceCard = ({ title, description, url="#" }) => {
   return (
-    <div className="group flex flex-col items-center rounded-[10px] px-2 py-[15px] transition hover:bg-primary active:scale-[0.99] md:w-[680px] md:px-[120px] md:py-[60px]">
+    <Link href={url} className="group flex flex-col items-center rounded-[10px] px-2 py-[15px] transition hover:bg-primary active:scale-[0.99] md:w-[680px] md:px-[120px] md:py-[60px]">
       <h3 className="mb-[10px] w-max text-center text-[20px] font-bold leading-normal text-gray-dark group-hover:text-white md:text-[36px]">
         {title}
       </h3>
       <p className="wrap-balance mt-[13px] text-center text-base font-light leading-normal text-gray-light group-hover:text-white md:text-[24px]">
         {description}
       </p>
-    </div>
+    </Link>
   );
 };
 
--- components/sections/Services.jsx
+++ components/sections/Services.jsx
@@ -9,27 +9,31 @@ const Services = () => {
       </h2>
 
       <div className="grid grid-cols-12 justify-center gap-[30px]">
-        <div className="col-span-12 flex items-center justify-center lg:col-span-6 ">
+        <div className="col-span-12 flex items-center justify-center lg:col-span-6">
           <ServiceCard
             title="Инвестировать"
+            url="/investor"
             description="Если вы хотите помочь в проведении инвестиционных сделок"
           />
         </div>
         <div className="col-span-12 flex items-center justify-center lg:col-span-6">
           <ServiceCard
             title="Разместить компанию"
+            url="/company"
             description="Информация для новых инвесторов о том, как устроен процесс сделок"
           />
         </div>
         <div className="col-span-12 flex items-center justify-center lg:col-span-6">
           <ServiceCard
             title="Стать партнером"
+            url="/become-partner"
             description="Если вы хотели бы помочь в проведении инвестиционных сделок"
           />
         </div>
         <div className="col-span-12 flex items-center justify-center lg:col-span-6">
           <ServiceCard
             title="Войти в личный кабинет"
+            url="/profile"
             description="Если вы уже зарегистрированы, то можете войти для изучения предложений"
           />
         </div>
