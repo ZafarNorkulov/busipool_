275451e0a75df6c732ccff058c369ca20efa4a39
ZafarNorkulov <norqulovzafar123@gmail.com>
2025-03-06 20:32:37 +0500

    fix:all build errors

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,10 +1,10 @@
 "use client";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 import { FaExclamationTriangle } from "react-icons/fa";
 
 const NotFoundPage = () => {
-  const router = useRouter();
+  const pathname = usePathname();
 
   return (
     <section className="flex-grow">
@@ -16,7 +16,7 @@ const NotFoundPage = () => {
           <div className="text-center">
             <h1 className="mb-2 mt-4 text-3xl font-bold">Page Not Found</h1>
             <p className="mb-10 text-xl text-gray-500">
-              {router.asPath.includes("/profile")
+              {pathname.includes("/profile")
                 ? "The page is not available in your role."
                 : "The page you are looking for does not exist."}
             </p>
