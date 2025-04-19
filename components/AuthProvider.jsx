f463492344da994feacc3c83512a3f9371fd1505
ZafarNorkulov <norqulovzafar123@gmail.com>
2025-04-19 14:56:35 +0500

    mongodb removed

--- components/AuthProvider.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-
-const AuthProvider = ({ children, session }) => {
-  return <SessionProvider session={session} basePath="/api/auth">{children}</SessionProvider>;
-};
-
-export default AuthProvider;
